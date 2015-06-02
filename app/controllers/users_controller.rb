261127ad55ba84675f3cfae7f05787c3de41dc12
tclewis91 <tclewis91@gmail.com>
2015-06-02 16:32:38 -0500

    map function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+  def index
+   u = User.all
+   u.map{|user| "#{first_name}, #{last_name}, #{occupation}, #{age}"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+get'/user', to: 'users#index'
+# get'/users', id: 'users#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
