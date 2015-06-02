624f8f53b74dec20a7160fb347eaa27004f0b01a
tclewis91 <tclewis91@gmail.com>
2015-06-02 17:41:56 -0500

    individual id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,13 @@ class UsersController < ApplicationController
   render text:
    u.map {|user| "#{user.first_name}, #{user.last_name}, #{user.occupation}, #{user.age}"}.join("<p>")
   end
+
+  def show
+    if User.exists?(params[:id])
+      user = User.find(params[:id])
+      render text: "#{user.first_name}, #{user.last_name}, #{user.age}"
+    else
+      render text: 'User Not Found', status: 404
+    end
+  end
 end
