61f680012769df00d329d2137f20c79ee7a7bfd3
tclewis91 <tclewis91@gmail.com>
2015-06-03 08:14:26 -0500

    comments

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,18 @@
 class UsersController < ApplicationController
   def index
 
-    if params != nil
+    # if params != nil
       # User.where("first_name LIKE ?", "#{params[:first_name]}")
-      User.select { |first_name| first_name.include?'s'}
-    end
+      # User.select{ |first| first_name.include?'s'," #{params[:first_name]}"}
+      #User.includes(:first_name).where("first.name = 's'
+      #adjust jacked if else statements
+    # end
 
-  #  @u = User.all
-  # render text:
-  #  @u.map {|user| "#{user.first_name}, #{user.last_name}, #{user.occupation}, #{user.age}"}.join("<p><img src = 'http://rlv.zcache.com/basset_hound_puppy_postcard-r4f199e398ea2471f9d5ea03cf3c44953_vgbaq_8byvr_50.jpg'/>")
-  # end
-end
+   @u = User.all
+  render text:
+   @u.map {|user| "#{user.first_name}, #{user.last_name}, #{user.occupation}, #{user.age}"}.join("<p><img src = 'http://rlv.zcache.com/basset_hound_puppy_postcard-r4f199e398ea2471f9d5ea03cf3c44953_vgbaq_8byvr_50.jpg'/>")
+  end
+
   def show
     if User.exists?(params[:id])
       user = User.find(params[:id])
@@ -19,6 +21,5 @@ end
       render text: 'Error: 404 User Not Found', status: 404
     end
   end
-
 end
 
